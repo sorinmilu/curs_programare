81858739a577ce320ca7fb566d310a8cb6f19c28
Dorian-Grim <irinel.buraga@s.utm.ro>
2020-11-21 09:26:26 +0200

    Update if-no-braces.c
    Interesant ar fi și exemplul comentat, dar, cu niște dar-uri.

--- Fundamentele programarii (Sem1)/Curs4/C/if-no-braces.c
+++ Fundamentele programarii (Sem1)/Curs4/C/if-no-braces.c
@@ -5,12 +5,15 @@ int main()
   int N;
   printf("numar? \n");
   scanf("%i",&N);
-
+  
   if (N > 0)
       printf("> decat zero\n");
   else
       printf("< decat zero\n");
-
+  
+  // însă nu putem folosi else după, trebuie neapărat inclus între {}
+  // if (N > 0) printf("1 linie");printf("2 linii");
+  
    printf("restul ...");
   return 0;
 }
